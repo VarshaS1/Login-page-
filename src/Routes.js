c13d4201d53a2511f0c9e5c49120062ee80f72f6
VarshaS-eng <varunvarsha85@gmail.com>
2020-01-24 13:59:27 +0530

    login functionlity

---
+++ src/Routes.js
@@ -0,0 +1,5 @@
+import Login from "./containers/Login";
+import Route from "react-router";
+import React  from 'react';
+
+<Route path="/login" exact component={Login} />
---
+++ src/containers/login.js
@@ -0,0 +1,103 @@
+/* eslint-disable no-undef */
+import React from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "./login.css";
+
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    console.log(event.target)
+    const { name, value } = event.target
+    this.setState({ [name]: value });
+
+    //this.setState({value: event.target.value});
+    //this.setState({value: event.target.value});
+
+  }
+
+
+  validation() {
+    let fields = this.state.fields;
+    let errors = {};
+    // eslint-disable-next-line no-unused-vars
+    let formIsValid = true;
+
+    //username
+    if (!fields["username"]) {
+      formIsValid = false;
+      errors["username"] = "*Please enter your username.";
+    }
+
+    if (typeof fields["username"] !== "undefined") {
+      if (!fields["username"].match(/^[a-zA-Z ]*$/)) {
+        formIsValid = false;
+        errors["username"] = "*Please enter alphabet characters only.";
+      }
+    }
+
+
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "*Please enter your password.";
+    }
+
+    if (typeof fields["password"] !== "undefined") {
+      if (!fields["password"].match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+        formIsValid = false;
+        errors["password"] = "*Please enter secure and strong password.";
+      }
+    }
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted');
+    event.preventDefault();
+  }
+
+  render() {
+    const { username, password } = this.state;
+    const isEnabled = username.trim().length > 0 && password.length > 0;
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="username">
+            <FormLabel>UserName</FormLabel>
+            <FormControl
+              autoFocus
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button type="submit" disabled={!isEnabled}>
+            Login
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default Login;
